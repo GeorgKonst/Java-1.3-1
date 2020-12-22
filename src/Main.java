fe8a0c065ad5ef7ab5bf5f7aee4d9397decfd28b
Georg Konst <Digitex71@mail.ru>
2020-12-22 14:17:49 +0300

    Mile

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 119_99;  //руб_коп
+        int mile = ticketPrice/20/100;
+
+        System.out.println(mile);
+    }
+}
